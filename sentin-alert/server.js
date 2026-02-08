f9c20735db408e79f08bfd73375cf6efac7903b3
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2026-02-08 02:02:10 +0200

    build(sentin-eye): dockerize python service (Closes MS4-Issue 6)
    - Created Dockerfile using python:3.9-slim base image to optimize size
    - Configured working directory and dependency installation via pip
    - Exposed port 5000 for Flask application access
    - Defined entrypoint command to launch app.py

--- sentin-alert/server.js
+++ sentin-alert/server.js
@@ -9,13 +9,52 @@ const PORT = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
+// In-Memory Storage temp place for storing a notifications
+let alerts = [];
+
 // 1. Health Check Route
 app.get('/status', (req, res) => {
     res.json({
         service: "Sentin-Alert (Node.js)",
         status: "online",
+        alerts_count: alerts.length,
         uptime: process.uptime()
     });
+});
+
+// 2. Trigger Alert (Endpoint that receives data from Python)
+app.post('/trigger', (req, res) => {
+    const { message, level, timestamp } = req.body;
+
+    // Validation
+    if (!message || !level) {
+        return res.status(400).json({ error: "Missing 'message' or 'level'" });
+    }
+
+    // Create Alert Object
+    const newAlert = {
+        id: alerts.length + 1,
+        message: message,
+        level: level, // e.g., "critical", "warning"
+        timestamp: timestamp || new Date().toISOString()
+    };
+
+    // Save to memory
+    alerts.push(newAlert);
+
+    // Log to console (Simulation of sending SMS/Email)
+    console.log(`[ALERT RECEIVED] Level: ${level} | Msg: ${message}`);
+
+    res.status(201).json({
+        success: true,
+        message: "Alert processed",
+        alert_id: newAlert.id
+    });
+});
+
+// 3. Get All Alerts (Endpoint for the Frontend Dashboard)
+app.get('/alerts', (req, res) => {
+    res.json(alerts);
 });
 
 // Start Server
